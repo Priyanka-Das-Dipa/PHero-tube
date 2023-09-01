d7b265d4b27f2489da426538ca8042212d5b9472
Priyanka-Das-Dipa <priyankadipa9102@gmail.com>
2023-09-01 16:57:07 +0600

    badge and catagory added

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const tubeCategory = async() => {
     data.data.slice(0, 4).forEach((category) => {
         const div = document.createElement('div')
         div.innerHTML = `
-        <a onclick="newsType('${category.category_id})" class="tab btn">${category.category}</a>
+        <a onclick="newsType('${category.category_id}')" class="tab btn">${category.category}</a>
         `
         tabContainer.appendChild(div)
     })
@@ -14,41 +14,48 @@ const tubeCategory = async() => {
 }
 
 // tab click data Show
-const newsType = async(categoryId) =>{
-    const response = await fetch('https://openapi.programming-hero.com/api/videos/category/1000')
+const newsType = async(categoryId) => {
+    const response = await fetch(`
+    https://openapi.programming-hero.com/api/videos/category/${categoryId}
+    `);
     const data = await response.json()
     const cardContainer = document.getElementById('card-container')
-    data?.data.forEach((videos) => {
+    cardContainer.innerHTML = ''
+    data?.data?.forEach((videos) => {
         const div = document.createElement('div')
         div.innerHTML = `
-        <div class="card w-96 bg-base-100 shadow-xl">
+        <div class="card bg-base-100 shadow-xl">
             <figure>
                 <img
-                src="https://i.ibb.co/M23fhxm/unsplash-Eh-Tc-C9s-YXsw.png"
-                alt="Shoes"
+                class="h-[200px]"
+                src=${videos?.thumbnail}
+                alt="Loading"
                 />
             </figure>
             <div class="flex px-3 gap-3 py-2">
                 <div class="avatar">
                     <div class="w-14 h-12 rounded-full">
                         <img
-                        src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80"
+                        src=${videos.authors[0]?.profile_picture}
                         />
                     </div>
                 </div>
                 <div>
                     <h2 class="card-title inter font-semibold text-lg text-[#171717]">
-                        Biden Pledges Nearly $3 Billion To Ukraine
+                        ${videos?.title}
                     </h2>
                     <div class="inter text-sm font-normal">
-                        <h6>Jimmy Dane</h6>
-                        <small>91k Views</small>
+                        <div class="flex gap-3">
+                            <h6>${videos?.authors[0]?.profile_name}</h6>
+                            <a href="">
+                            ${videos?.authors[0]?.verified === true ? ' <img src="./image/fi_10629607.png" alt="">' : ''}
+                            </a>
                         </div>
+                        <small>${videos?.others?.views}</small>
+                    </div>
                 </div>
             </div>
         </div>
-        
-        
         `
         cardContainer.appendChild(div)
 
@@ -56,4 +63,6 @@ const newsType = async(categoryId) =>{
 
 
 }
+
 tubeCategory()
+newsType('1000')
